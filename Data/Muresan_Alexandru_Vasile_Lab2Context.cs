b7d62bec2d000aa08494f7d4d82e77a13cdffe28
murisano@gmail.com <murisano@gmail.com>
2022-11-10 00:36:58 +0200

    Add project files.

---
+++ Data/Muresan_Alexandru_Vasile_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Muresan_Alexandru_Vasile_Lab2.Models;
+
+namespace Muresan_Alexandru_Vasile_Lab2.Data
+{
+    public class Muresan_Alexandru_Vasile_Lab2Context : DbContext
+    {
+        public Muresan_Alexandru_Vasile_Lab2Context (DbContextOptions<Muresan_Alexandru_Vasile_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Muresan_Alexandru_Vasile_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Muresan_Alexandru_Vasile_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Muresan_Alexandru_Vasile_Lab2.Models.Author> Author { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Muresan_Alexandru_Vasile_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string AuthorName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+public ICollection<Book>? Books { get; set; } //navigation property
+    }
+}
