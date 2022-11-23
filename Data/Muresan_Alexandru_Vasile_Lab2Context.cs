8a114d884d807f00bcb33b6124ffcf55b3dd96bc
murisano@gmail.com <murisano@gmail.com>
2022-11-23 23:52:39 +0200

    Laborator 5 pana la  Razor Identity

--- Data/Muresan_Alexandru_Vasile_Lab2Context.cs
+++ Data/Muresan_Alexandru_Vasile_Lab2Context.cs
@@ -21,5 +21,9 @@ namespace Muresan_Alexandru_Vasile_Lab2.Data
         public DbSet<Muresan_Alexandru_Vasile_Lab2.Models.Author> Author { get; set; }
 
         public DbSet<Muresan_Alexandru_Vasile_Lab2.Models.Category> Category { get; set; }
+
+        public DbSet<Muresan_Alexandru_Vasile_Lab2.Models.Member> Member { get; set; }
+
+        public DbSet<Muresan_Alexandru_Vasile_Lab2.Models.Borrowing> Borrowing { get; set; }
     }
 }
---
+++ LibraryIdentityContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Muresan_Alexandru_Vasile_Lab2
+{
+    public class LibraryIdentityContext
+    {
+    }
+}
